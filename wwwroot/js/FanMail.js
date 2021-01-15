4efc277a527320f85a0e3bd0f83e60fb7856a06a
scottsyoung <scottsy@byu.edu>
2021-01-15 11:52:19 -0700

    assignment to turn in

--- wwwroot/js/FanMail.js
+++ wwwroot/js/FanMail.js
@@ -1,7 +1,57 @@
 ﻿var submit = document.getElementById("submitButton")
 submit.addEventListener("click", function () {
-    alert("From: " + document.getElementById("from").value + ", " +
-        "Email: " + document.getElementById("email").value + ", " +
-        "subject: " + document.getElementById("subject").value + ", " +
-        "Message: " + document.getElementById("message").value);
+
+    var assignments = Number(document.getElementById("assignments").value)
+    var groupProject = Number(document.getElementById("groupProj").value)
+    var quizzes = Number(document.getElementById("quizzes").value)
+    var exams = Number(document.getElementById("exams").value)
+    var intex = Number(document.getElementById("intex").value)
+
+    var finalPercent = (assignments * .5) + (groupProject * .1) +
+        (quizzes * .1) + (exams * .2) + (intex * .1);
+
+    var finalGrade = "";
+
+    if (finalPercent >= 94) {
+        finalGrade = "A"
+    }
+    else if (finalPercent >= 90) {
+        finalGrade = "A-"
+    }
+    else if (finalPercent >= 87) {
+        finalGrade = "B+"
+    }
+    else if (finalPercent >= 84) {
+        finalGrade = "B"
+    }
+    else if (finalPercent >= 80) {
+        finalGrade = "B-"
+    }
+    else if (finalPercent >= 77) {
+        finalGrade = "C+"
+    }
+    else if (finalPercent >= 74) {
+        finalGrade = "C"
+    }
+    else if (finalPercent >= 70) {
+        finalGrade = "C-"
+    }
+    else if (finalPercent >= 67) {
+        finalGrade = "D+"
+    }
+    else if (finalPercent >= 64) {
+        finalGrade = "D"
+    }
+    else if (finalPercent >= 60) {
+        finalGrade = "D-"
+    }
+    else {
+        finalGrade = "E"
+    }
+
+    finalPercent = String(finalPercent);
+    finalString = finalGrade + " " + finalPercent + "%"
+
+    alert(finalString);
+
 })
